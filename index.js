c845554120f12ee0542b98e803da2dcf0b1513cd
Ronald Shumery <ronaldshumery@gmail.com>
2020-07-29 16:51:03 -0400

    ajustes para deployment}

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json({ extended: true})); //Enviar como header/json
 //Se utiliza el puerto 4000 para que no choque con el 3000 del cliente 
 //Heroku busca la variable PORT por defecto
 //Puerto de la app
-const PORT = process.env.PORT || 4000;
+const port = process.env.port || 4000;
 
 //Importar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
@@ -27,6 +27,6 @@ app.use('/api/tareas', require('./routes/tareas'));
 
 
 //Iniciar la app
-app.listen(PORT, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`El servidor esta funcionando en el puerto ${PORT}`);
 })
